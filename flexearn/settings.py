1e1f6be281fbca5793ac6b90848d7e21c6dbb1bf
jonathan <jonathaonderi2018@yahoo.com>
2020-11-26 08:21:54 +0300

    commit

--- flexearn/settings.py
+++ flexearn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'n$)@lo*1$q8emsv@)qw-an7vl)s(&@v^lemlq(f21#b(jzc)b$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
